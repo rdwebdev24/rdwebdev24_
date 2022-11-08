16d77652a3bf174008bdf9a0390200f43729e54c
rdwebdev24 <rohitdhakad101@gmail.com>
2022-11-08 14:26:25 +0530

    inital commit 2.0

--- js/index.mjs
+++ js/index.mjs
@@ -14,10 +14,11 @@ let cursor = document.querySelector(".cursor");
 let  typeAudio = document.getElementById("myAudio")
 let  typeAudio2 = document.getElementById("myAudio2")
 typeAudio2.currentTime=2;
+typeAudio2.startDelay=3
 typeAudio2.play();
 
 
- 
+
 playbutton.addEventListener('click',()=>{
   playaudio.style.animation = "glow_audio 2s infinite forwards"
   playbutton.style.display = 'none'
